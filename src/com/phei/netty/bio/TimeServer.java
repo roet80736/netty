9aa70502eeea97250b437704238c89e5bfd6d6b9
西祠梧桐 <919964333@qq.com>
2017-07-24 15:21:06 +0800

    one commit

---
+++ src/com/phei/netty/bio/TimeServer.java
@@ -0,0 +1,23 @@
+package com.phei.netty.bio;
+
+import java.net.ServerSocket;
+
+public class TimeServer {
+
+    public static void main(String[] args) {
+	int port = 8080;
+	if(args != null && args.length > 0){
+	    port = Integer.valueOf(args[0]);
+	}
+	ServerSocket server = null;
+	
+	
+	
+	
+	
+    }
+    
+    
+    
+    
+}
